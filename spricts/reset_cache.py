cde5c65e16116bf75b8783061aeb078c0f6eacca
rice-powder <ricepowder320@gmail.com>
2026-08-26 02:42:55 +0900

    index.htmlのスタイルシートリンクにバージョン番号を追加し、reset_cache.pyを新規作成してバージョン管理を実装

---
+++ spricts/reset_cache.py
@@ -0,0 +1,14 @@
+import re
+from pathlib import Path
+
+path = Path(r"../index.html")
+
+text: str = path.read_text(encoding="utf-8")
+
+def update_version(match) -> str:
+    version = int(match.group(1))
+    return f"?ver={version + 1}"
+
+text = re.sub(pattern=r"\?ver=(\d+)", repl=update_version, string=text)
+
+path.write_text(data=text, encoding="utf-8")
